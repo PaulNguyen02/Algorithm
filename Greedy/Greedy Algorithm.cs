9f3dac5d9294406e57ff6c140d228ce6752ea792
Phúc Nguyễn <105428358+PaulNguyen02@users.noreply.github.com>
2022-11-23 07:25:41 -0800

    Add files via upload
    Thuật toán tham lam

---
+++ Greedy/Greedy Algorithm.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static int []value = { 500000, 200000, 100000, 50000, 20000, 10000, 5000, 2000, 1000 };
+        static void Greedy(int M)
+        {
+            int count;
+            for(int i=0; i< value.Length ;i++)
+            {
+                count = 0;
+                while(M>=value[i])
+                {
+                    M -= value[i];
+                    count++;
+                    Console.WriteLine(value[i] + " " + count);
+                }
+                
+            }
+        }
+        static void Main(string[] args)
+        {
+            Greedy(155000);
+            Console.ReadKey();
+        }
+    }
+}
